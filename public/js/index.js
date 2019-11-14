77db743257c22d66940b5c7d20377d7e033d68ef
Akhil <akhilvijayan004@gmail.com>
2019-11-14 20:37:57 +1100

    add loader

--- public/js/index.js
+++ public/js/index.js
@@ -74,21 +74,21 @@ $(document).keyup(function(e) {
 // LOGIN + ACCOUNT CREATION
 
 $(function() {
-    document.getElementById('navbarCollapse').click();
     var game = $("<canvas id='game-canvas'></canvas>");
     game.addClass("border rounded");
-    
     if (getCookie("debugger") == "true") {
         $('.account-button').hide();
         $('.logout-button').show();
         $(".game-main").html(game);
         var user = getCookie("username");
         $('.game-user').html(user);
+        $('#loading').addClass("d-none");
     } else {
         $('.account-button').show();
         $('.logout-button').hide();
         $(".game-main").html('');
         $('.user-header').hide();
+        document.getElementById('navbarCollapse').click();
     }
 
     $(".create-submit").on("click", function(event) {
