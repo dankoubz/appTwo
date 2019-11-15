30429a0c51745964911901058122b37400edaad0
Akhil <akhilvijayan004@gmail.com>
2019-11-16 09:41:37 +1100

    fix the leaderboard issue

--- public/js/index.js
+++ public/js/index.js
@@ -162,11 +162,6 @@ $(function() {
                         res[i].total_score + '</td></tr>';
                 }
                 $('.leader-table').append(content);
-                for (i = 0; i < res.length; i++) {
-                    content += '<tr><td>' + res[i].username + '</td><td>' +
-                        res[i].total_score + '</td></tr>';
-                }
-                $('.leader-table').append(content);
             }
         );
     });
